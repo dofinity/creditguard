f99849102545eea0b06a35cba9d3b43f62c2f893
Yonatan Rab <yonatan.rab@gmail.com>
2018-11-26 07:17:36 +0000

    Added default namespace when generating wsdl

--- generateWsdl.php
+++ generateWsdl.php
@@ -5,6 +5,7 @@ $generator = new \Wsdl2PhpGenerator\Generator();
 $generator->generate(
   new \Wsdl2PhpGenerator\Config(array(
     'inputFile' => $_GET['wsdl_path'],
-    'outputDir' => './wsdl'
+    'outputDir' => './wsdl',
+    'namespaceName' => 'Creditguard\Wsdl'
   ))
 );

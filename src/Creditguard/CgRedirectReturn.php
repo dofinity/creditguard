38a2bc139f677b88563b5645f38d1ee2062a351d
reissr <rotem@dofinity.com>
2018-12-25 12:01:09 +0200

    Validation improvements

--- src/Creditguard/CgRedirectReturn.php
+++ src/Creditguard/CgRedirectReturn.php
@@ -17,13 +17,12 @@ class CgRedirectReturn {
   protected $cardMask;
   protected $errorText;
 
+  protected $validation_errors;
+
   /**
    * CgRedirectReturn constructor.
    * @param array $queryParams
    *   Associative array of all parameters received from CG
-   *
-   * @return array
-   *   Returns array of validation error messages or empty array if non
    */
   public function __construct($queryParams) {
     // Init the main parameters
@@ -32,37 +31,37 @@ class CgRedirectReturn {
     $this->cardExp = !empty($_GET['cardExp']) ? $_GET['cardExp'] : NULL;
     $this->authNum = !empty($_GET['authNumber']) ? $_GET['authNumber'] : NULL;
     $this->cardMask = !empty($_GET['cardMask']) ? $_GET['cardMask'] : NULL;
-    $this->errorText = !empty($_GET['ErrorText']) ? $_GET['ErrorText'] : '';
+    $this->errorText = !empty($_GET['ErrorText']) ? htmlspecialchars($_GET['ErrorText'], ENT_QUOTES, 'UTF-8') : '';
 
-    return $this->isValidParams();
+    $this->checkParamsValidity();
   }
 
   /**
    * @return string
    */
   public function getErrorCode() {
-    return $this->errorCode;
+    return $this->isParamValid('errorCode') ? $this->errorCode : FALSE;
   }
 
   /**
    * @return string
    */
   public function getTxId() {
-    return $this->txId;
+    return $this->isParamValid('txId') ? $this->txId : FALSE;
   }
 
   /**
    * @return string
    */
   public function getCardExp() {
-    return $this->cardExp;
+    return $this->isParamValid('cardExp') ?  $this->cardExp : FALSE;
   }
 
   /**
    * @return string
    */
   public function getAuthNum() {
-    return $this->authNum;
+    return $this->isParamValid('authNum') ?  $this->authNum : FALSE;
   }
 
   /**
@@ -77,54 +76,83 @@ class CgRedirectReturn {
    * @return bool|string
    */
   public function getLastDigits() {
-    return substr($this->cardMask, -4);
+    return $this->isParamValid('cardMask') ? substr($this->cardMask, -4) : FALSE;
+  }
+
+  /**
+   * Check if the given param name has validation errors
+   * @param $paramName
+   *
+   * @return bool
+   *   True for valid, false for invalid
+   */
+  public function isParamValid($paramName) {
+    return !key_exists($paramName, $this->validation_errors);
+  }
+
+  /**
+   * Array of validation messages where the key is the param name
+   * @return array
+   *   Messages or empty array in case of no validation errors
+   */
+  public function getValidationErrors() {
+    return $this->validation_errors;
   }
 
   /**
    * Helper function to check if the parameters are safe to use
    *
    * @return array
-   *   Messages or empty array in case of no validation erros
+   *   Messages or empty array in case of no validation errors
    */
-  public function isValidParams() {
-    $messages = [];
-    $required_fields = [
-      'txId',
-      'cardMask',
-      'cardExp'
-    ];
-
-    $numeric_fields = [
-      'errorCode',
-      'cardExp',
-      'authNumber'
-    ];
+  private function checkParamsValidity() {
+    $this->validation_errors = [];
 
     // Check for required fields
-    foreach ($required_fields as $required_field) {
+    foreach ($this->getRequiredParamNames() as $required_field) {
       if (empty($this->{$required_field})) {
-        $messages[] = "Required field is missing $this->{$required_field} query parameter";
+        $this->validation_errors[$required_field] = "Required field is missing $this->{$required_field} query parameter";
       }
     }
 
     // Check the numeric fields
-    foreach ($numeric_fields as $numeric_field) {
-      $value = $this->{$numeric_field};
+    foreach ($this->getNumericParamNames() as $numeric_param_name) {
+      $value = $this->{$numeric_param_name};
       if (!empty($value) && !is_numeric($value)) {
-        $messages[] = "Illegal value in $this->{$numeric_field} query parameter";
+        $this->validation_errors[$numeric_param_name] = "Illegal value in $this->{$numeric_param_name} query parameter";
       }
     }
 
     // Check the card mask (last 4 digits)
     if (!is_numeric($this->getLastDigits())) {
-      $messages[] = "Illegal value in cardMask query parameter";
+      $this->validation_errors['cardMask'] = "Illegal value in cardMask query parameter";
     }
 
     // Validate the transaction ID
     if (!preg_match('/^[a-zA-Z0-9\-]*$/', $this->txId)) {
-      $messages[] = "Illegal value in txId query parameter";
+      $this->validation_errors['txId'] = "Illegal value in txId query parameter";
     }
+  }
 
-    return $messages;
+  /**
+   * @return array
+   */
+  private function getRequiredParamNames() {
+    return [
+      'txId',
+      'cardMask',
+      'cardExp'
+    ];
+  }
+
+  /**
+   * @return array
+   */
+  private function getNumericParamNames() {
+    return [
+      'errorCode',
+      'cardExp',
+      'authNumber'
+    ];
   }
 }
